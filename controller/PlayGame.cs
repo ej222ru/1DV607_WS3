863ee8ff824f7a388a83cc5237a724612cab3485
Erland Jönsson <ej222ru@student.lnu.se>
2015-10-10 20:22:06 +0200

    playable

---
+++ controller/PlayGame.cs
@@ -0,0 +1,35 @@
+﻿using BlackJack.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.controller
+{
+    class PlayGame
+    {
+        public bool Play(model.Game a_game, view.IView a_view)
+        {
+            a_view.DisplayWelcomeMessage();
+            
+            a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
+            a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+
+            if (a_game.IsGameOver())
+            {
+                a_view.DisplayGameOver(a_game.IsDealerWinner());
+            }
+
+            UserAction userAction = a_view.GetUserAction();
+            switch (userAction)
+            {
+                case UserAction.Play :  { a_game.NewGame(); break; }
+                case UserAction.Hit :   { a_game.Hit(); break; }
+                case UserAction.Stand : { a_game.Stand(); break; }
+                case UserAction.Quit :  { return false; }
+                default: break;    
+            }
+            return true;
+        }
+    }
+}
---
+++ view/IView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.view
+{
+    public enum UserAction { Play = 1, Hit, Stand, Quit };
+    interface IView
+    {
+        void DisplayWelcomeMessage();
+        UserAction GetUserAction();
+        void DisplayCard(model.Card a_card);
+        void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score);
+        void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score);
+        void DisplayGameOver(bool a_dealerIsWinner);
+    }
+}
